a57154127b1e3490cb1e2bef1e501aebb4a15d55
abdllah <abdallahabusalama0@gmail.com>
2024-12-14 12:40:12 +0200

    Initial Commit

---
+++ project7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project7
+//
+//  Created by abdallah abu salama on 14/12/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
